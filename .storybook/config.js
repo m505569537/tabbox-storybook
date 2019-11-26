b2881f5780cacab75c839bd1e044cf225c713e2c
xuhaoshun <xuhaoshun@yiwei.com>
2019-11-26 14:18:22 +0800

    taskbox UI

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+import '../src/index.css';
+
+// automatically import all files ending in *.stories.js
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+const req = requireContext('../src', true, /\.stories\.js$/);
+
+function loadStories () {
+    req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
